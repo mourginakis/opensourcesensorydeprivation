8db465c7a75994fe468ca12114bb3a30c9ed4e88
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2024-11-26 01:30:19 -0800

    switch to fahrenheit

--- arduino/brain/brain.ino
+++ arduino/brain/brain.ino
@@ -10,42 +10,26 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 // is connected to digital pin 2
 #define ONE_WIRE_BUS 2
 
-// Define the pin connected to the SSR
-// Other side of the SSR is gonnected to GND
+// Pin 8 = SSR, other pin connected to GND
 // Constrain temp to a range to reduce likeliness of always-on
 // An unplugged sensor is like -127 degrees
-float tempMin = 15.0;
-float tempMax = 34.28;
+float tempMinF = 59.0;
+float tempMaxF = 93.7;
 const int ssrPin = 8;
+
+const int potPin = A0;
+
+// Like arduino map but for floats.
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 
 // Create a OneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);
 
 // Pass the oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
-
-
-/*
-Fahrenheit | Celsius
------------|---------
- 60.0.     | 15.55 
- 93.0      | 33.89
- 93.1      | 33.94
- 93.2      | 33.99
- 93.3      | 34.06
- 93.4      | 34.11
- 93.5      | 34.17
- 93.6      | 34.22
- 93.7      | 34.28
- 93.8      | 34.33
- 93.9      | 34.39
- 94.0      | 34.44
- 97.0      | 36.11
- 98.6.     | 37.00
-
-Note: Celsius = (Fahrenheit - 32) * 5 / 9
-*/
-
 
 
 void setup() {
@@ -66,17 +50,37 @@ void setup() {
 void loop() {
 
   sensors.requestTemperatures(); // Request temperature measurement
-  
-  // Read temperature in Celsius
-  float temperatureC = sensors.getTempCByIndex(0);
+  float temperatureF = sensors.getTempFByIndex(0);
+
+  // 0 - 1023?
+  int potValue = analogRead(potPin);
+  // map only works with integers.
+  float mappedValue = (potValue / 1023.0) * 6.0 - 3.0;
+  float targetTempF = tempMaxF + mappedValue;
+
+
+  for (int i = 0; i < 20; i++) {
+    // we put this in an internal loop bc the sensor bounces a lot
+    // and we don't want to switch on and off the heater every second.
+    // TODO: fix this (maybe use a deque and an average)
+    sensors.requestTemperatures();
+    temperatureF = sensors.getTempFByIndex(0);
+    potValue = analogRead(potPin);  // 0 - 1023?
+    mappedValue = (potValue / 1023.0) * 8.0 - 4.0; // map only works with integers
+    targetTempF = tempMaxF + mappedValue;
+
+    lcd.setCursor(0, 0);    // Set cursor to start of first row
+    lcd.print("f: " + String(temperatureF) + " -> " + String(targetTempF) + "    ");
+    delay(1000);
+  }
   
   // Display the temperature
-  Serial.println("Temperature: " + String(temperatureC) + " °C");
+  Serial.println("Temperature: " + String(temperatureF) + " °F");
 
   int ssrStatus = LOW;
 
 
-  if (tempMin < temperatureC && temperatureC < tempMax) {
+  if (tempMinF < temperatureF && temperatureF < targetTempF) {
     // digitalWrite(ssrPin, HIGH);
     ssrStatus = HIGH;
   } else {
@@ -88,12 +92,12 @@ void loop() {
 
   lcd.clear();            // Clear lcd (not necessary but nice)
   lcd.setCursor(0, 0);    // Set cursor to start of the second row
-  lcd.print("temp_c: " + String(temperatureC) + "    ");   // Print the label
+  lcd.print("f: " + String(temperatureF) + " -> " + String(targetTempF) + "    ");   // Print the label
   // lcd.print(temperatureC);     // Display the counter value
   // lcd.print("    ");      // Clear extra characters
   lcd.setCursor(0, 1);
   lcd.print("heater: " + String(ssrStatus) + "    ");
 
-  delay(1000 * 20); // wait 20 second
+  delay(1000); // wait 1 second
   // delay(1000 * 60); // Wait 60 seconds before taking another reading
 }
