51920af03a63700545f31dbea26f70623e9abd32
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2025-02-02 18:26:45 -0800

    get wifi server example working

---
+++ arduino/brain2/README.md
@@ -0,0 +1,9 @@
+### WIFI Credentials Store
+
+Create a file in this directory
+
+```c
+# arduino_secrets.h
+#define SECRET_SSID "wifi_network_name"
+#define SECRET_PASS "wifi_password"
+```
---
+++ arduino/brain2/brain2.ino
@@ -0,0 +1,136 @@
+/*
+Hosts an HTTP server over WIFI. WPA/WPA2 only.
+
+Find the full UNO R4 WiFi Network documentation here:
+https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples#simple-webserver
+
+You can edit this in VSCode with AI and use the arduino IDE to upload it 
+to the board. The IDE will live update the file.
+
+*/
+
+#include "WiFiS3.h"
+
+#include "arduino_secrets.h"
+
+
+char ssid[] = SECRET_SSID;        // network SSID
+char pass[] = SECRET_PASS;        // network password
+
+int led =  LED_BUILTIN;
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+
+
+void setup() {
+  Serial.begin(9600);
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network.
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  server.begin();
+  // you're connected now, so print out the status:
+  printWifiStatus();
+
+}
+
+
+void loop() {
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out to the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("<p style=\"font-size:7vw;\">Click <a href=\"/H\">here</a> turn the LED on<br></p>");
+            client.print("<p style=\"font-size:7vw;\">Click <a href=\"/L\">here</a> turn the LED off<br></p>");
+            
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+        }
+      }
+      
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+
+
+// Data wire of DS18B20 Temperature Sensor (digital pin 2)
+// #define ONE_WIRE_BUS 2
+
+// Create a OneWire instance to communicate with any OneWire device
+// OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass the oneWire reference to Dallas Temperature.
+// DallasTemperature sensors(&oneWire);
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
